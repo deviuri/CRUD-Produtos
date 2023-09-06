60e64d0e6a14e248dc4156ee0a4b0e6c2a49e88b
Iuri Martins <iurim134@gmail.com>
2023-09-06 10:23:07 -0300

    a

--- backend/src/main/java/com/test/crud/Repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.test.crud.Repository;
-
-import com.test.crud.Model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
